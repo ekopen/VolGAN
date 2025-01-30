0d184cc051f5386bd0d03a1bc03a2d27a8673ce5
Derek Li <derekli@Eriks-MacBook-Pro-2.local>
2025-01-30 00:33:12 -0600

    Adding one helper function for reading excel sheets

--- Inputs.py
+++ Inputs.py
@@ -6,6 +6,49 @@ import seaborn as sns
 import warnings
 warnings.filterwarnings("ignore")
 
+# Helper Functions
+
+def read_excel_default(excel_name: str, index_col : int = 0, parse_dates: bool =True, print_sheets: bool = False, sheet_name: str = None, **kwargs):
+    """
+    Reads an Excel file and returns a DataFrame with specified options.
+
+    Parameters:
+    excel_name (str): The path to the Excel file.
+    index_col (int, default=0): Column to use as the row labels of the DataFrame.
+    parse_dates (bool, default=True): Boolean to parse dates.
+    print_sheets (bool, default=False): If True, prints the names and first few rows of all sheets.
+    sheet_name (str or int, default=None): Name or index of the sheet to read. If None, reads the first sheet.
+    **kwargs: Additional arguments passed to `pd.read_excel`.
+
+    Returns:
+    pd.DataFrame: DataFrame containing the data from the specified Excel sheet.
+
+    Notes:
+    - If `print_sheets` is True, the function will print the names and first few rows of all sheets and return None.
+    - The function ensures that the index name is set to 'date' if the index column name is 'date' or 'dates', or if the index contains date-like values.
+    """
+    if print_sheets:
+        n = 0
+        while True:
+            try:
+                sheet = pd.read_excel(excel_name, sheet_name=n)
+                print(f'Sheet {n}:')
+                print(", ".join(list(sheet.columns)))
+                print(sheet.head(3))
+                n += 1
+                print('\n' * 2)
+            except:
+                return
+    sheet_name = 0 if sheet_name is None else sheet_name
+    returns = pd.read_excel(excel_name, index_col=index_col, parse_dates=parse_dates,  sheet_name=sheet_name, **kwargs)
+    if returns.index.name is not None:
+        if returns.index.name.lower() in ['date', 'dates']:
+            returns.index.name = 'date'
+    elif isinstance(returns.index[0], (datetime.date, datetime.datetime)):
+        returns.index.name = 'date'
+    return returns
+
+
 def maturity_tenor(filename):
     mat_n_ten = pd.read_excel(filename, nrows=2)
     new_header = mat_n_ten.iloc[1]
@@ -131,3 +174,4 @@ def tabular_rvol_form(date, nperiods, ann, filename = "forward_sofr_swap_full.xl
     df = realized_volatility_data(date, nperiods, ann)
     grid = df.pivot(index='Tenor', columns="Maturity", values='Values')
     return grid
+
