d0ec5a8e5fe0838278a1c329079edbe5eb50a3f6
eotero1 <eotero@uchicago.edu>
2025-03-13 00:29:41 -0400

    Update Prices and Greeks.py

--- Prices and Greeks.py
+++ Prices and Greeks.py
@@ -150,13 +150,14 @@ class Forward_Swap:
         return self.forward_floating_bond() - self.forward_bond()
 
 
-filename = "generated_surfaces_test_new.csv"
+filename = "generated_surfaces_mean.csv"
 filepath = "forward_sofr_swap_full.xlsx"
 filepath2 = "swaption_atm_vol_full.xlsx"
 gen_s = pd.read_csv(filename, skiprows=2).set_index("Ticker")
 forward_swap = pd.read_excel(filepath, skiprows=2).set_index("Ticker").sort_index()
 atm_vol = pd.read_excel(filepath2, skiprows=2).set_index("Ticker").sort_index()
-returns = pd.read_excel("generated_returns.xlsx", skiprows=2).set_index("Ticker").iloc[1:, :]/100
+returns = pd.read_csv("generated_returns_mean.csv", skiprows=2).set_index("Ticker").iloc[1:, :]/100
+returns.index = pd.to_datetime(returns.index)
 mat_n_ten1 = maturity_tenor("generated_surfaces_test_new.csv").T
 
 def new_prices(date, base_date=None):
@@ -196,7 +197,7 @@ def new_prices(date, base_date=None):
     
     df2 = realized_prices(date, base_date=base_date)
     mdf = df2.join(df[["New Price"]]).dropna()
-    mdf["Pred PnL"] = mdf["New Price"] - mdf["Price"]
+    mdf["Pred PnL"] = mdf["Price"] - mdf["New Price"]
     return mdf[["Tenor", "Maturity", "Pred PnL"]]
 
 def all_prices(date):
